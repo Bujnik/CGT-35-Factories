4bcace1c72a023e999c76287f3cce68e559d0e92
Dawid Bujnicki <89477976+Bujnik@users.noreply.github.com>
2022-02-15 11:58:14 +0100

    Part 2
    Created MaleFactory, returns object based on its age

--- src/main/Solution.java
+++ src/main/Solution.java
@@ -1,7 +1,12 @@
 package main;
 
+import main.man.MaleFactory;
+
 public class Solution {
     public static void main(String[] args) {
-
+        MaleFactory maleFactory = new MaleFactory();
+        System.out.println(maleFactory.getPerson(99));
+        System.out.println(maleFactory.getPerson(4));
+        System.out.println(maleFactory.getPerson(15));
     }
 }
---
+++ src/main/man/MaleFactory.java
@@ -0,0 +1,11 @@
+package main.man;
+
+import main.Human;
+
+public class MaleFactory {
+    public Human getPerson(int age){
+        if (age < 13) return new KidBoy();
+        else if (age < 20) return new TeenBoy();
+        else return new Man();
+    }
+}
